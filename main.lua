77d2f665f9c5422009a746baf979c4919a8a488d
Cgull <tomas.motiejunas01@gmail.com>
2023-12-22 18:31:02 +0200

    shooting

--- main.lua
+++ main.lua
@@ -4,67 +4,75 @@ local Utils = require("utils")
 
 windowWidth, windowHeight = love.graphics.getDimensions()
 
-
 function love.load()
-
     world = love.physics.newWorld(0, 0, true)
     gameOver = false
+    bulletSpeed = 250
+
+    -- create the player
     player = {}
     player.angle = 0
-    player.body = love.physics.newBody(world, windowWidth/2, windowHeight/2, "dynamic")
-    player.shape = love.physics.newPolygonShape(-10, -10, 10, 0, -10, 10,-5, 0)
+    player.body = love.physics.newBody(world, windowWidth / 2, windowHeight / 2, "dynamic")
+    player.shape = love.physics.newPolygonShape(-10, -10, 10, 0, -10, 10, -5, 0)
     player.fixture = love.physics.newFixture(player.body, player.shape) -- connect body to shape
     player.fixture:setUserData("Player")
+    player.bullets = {}
 
+    -- create one asteroid
     asteroid = {}
-    asteroid.body = love.physics.newBody(world, windowWidth,windowHeight/2, "dynamic")
+    asteroid.body = love.physics.newBody(world, windowWidth, windowHeight / 2, "dynamic")
     asteroid.shape = love.physics.newCircleShape(20)
     asteroid.fixture = love.physics.newFixture(asteroid.body, asteroid.shape)
     asteroid.fixture:setUserData("Asteroid")
 
-    bulletSpeed = 250
-    player.bullets = {}
-
     world:setCallbacks(beginContact, endContact, preSolve, postSolve)
-
-
 end
 
 function beginContact(a, b, coll)
     local x, y = coll:getNormal()
-	local textA = a:getUserData()
-	local textB = b:getUserData()
+    local textA = a:getUserData()
+    local textB = b:getUserData()
 
     if textA == "Player" and textB == "Asteroid" or textB == "Player" and textA == "Asteroid" then
         gameOver = true
     end
 
-
     print(textA, textB)
 end
 
-function endContact(a, b, coll)
+function love.mousepressed(x, y, button)
+    if button == 2 then -- if right mouse button pressed, spawn a bullet
+        local startX = player.body:getX() + 10 / 2
+        local startY = player.body:getY() + 10 / 2
+
+        local angle = math.atan2((y - startY), (x - startX))
+
+        local bulletDx = bulletSpeed * math.cos(angle)
+        local bulletDy = bulletSpeed * math.sin(angle)
+
+        table.insert(player.bullets, {x = startX, y = startY, dx = bulletDx, dy = bulletDy})
+    end
 end
-
-function preSolve(a, b, coll)
-
-end
-
-function postSolve(a, b, coll, normalimpulse, tangentimpulse)
-
-end
-
 
 function love.update(dt)
-
     world:update(dt)
 
     local mouseX, mouseY = love.mouse.getPosition()
-
     dirX, dirY = Utils.getDirectionToPoint(player.body:getX(), player.body:getY(), mouseX, mouseY)
 
-    if love.mouse.isDown(1) then
-        player.body:applyForce(dirX*250, dirY*250)
+    if love.mouse.isDown(1) then -- if left mouse button is pressed, move the player
+        player.body:applyForce(dirX * 150, dirY * 150)
+    end
+
+    -- move the bullets and remove the ones out of bounds
+    for i = #player.bullets, 1, -1 do
+        local bullet = player.bullets[i]
+        bullet.x = bullet.x + (bullet.dx * dt)
+        bullet.y = bullet.y + (bullet.dy * dt)
+
+        if bullet.x < 0 or bullet.y < 0 or bullet.x > windowWidth or bullet.y > windowHeight then
+            table.remove(player.bullets, i)
+        end
     end
 
     player.angle = math.atan2(dirY, dirX)
@@ -75,43 +83,41 @@ function love.update(dt)
     player.body:setX(Px % windowWidth)
     player.body:setY(Py % windowHeight)
 
-
     local Ax = asteroid.body:getX()
     local Ay = asteroid.body:getY()
 
     asteroid.body:setX(Ax % windowWidth)
     asteroid.body:setY(Ay % windowHeight)
-
-
-    -- asteroid.x = (asteroid.x + asteroid.Vx * dt * 200) % windowWidth
-    -- asteroid.y = (asteroid.y + asteroid.Vy * dt * 200) % windowHeight
-
-    -- player.x = (player.x + player.Vx * dt * 200) % windowWidth
-    -- player.y = (player.y + player.Vy * dt * 200) % windowHeight
 end
 
 function love.draw()
-
     local angle = math.atan2(dirY, dirX)
 
-    love.graphics.push() -- Save the current transformation
-    love.graphics.translate(player.body:getX(), player.body:getY()) -- Move the origin to the player's position
-    love.graphics.rotate(player.angle) -- Rotate the coordinate system
-    love.graphics.polygon("line", -10, -10, 10, 0, -10, 10,-5, 0) -- Draw the triangle
+    -- draw bullets
+    love.graphics.push()
+    love.graphics.setColor(1,1,1)
+    for i, v in ipairs(player.bullets) do
+        love.graphics.circle("fill", v.x, v.y, 1)
+    end
     love.graphics.pop()
 
-    love.graphics.push() -- Save the current transformation
-    love.graphics.circle("line",asteroid.body:getX(), asteroid.body:getY(), 20,5) -- Draw the triangle
+    -- draw player
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.push()
+    love.graphics.translate(player.body:getX(), player.body:getY())
+    love.graphics.rotate(player.angle)
+    love.graphics.polygon("line", -10, -10, 10, 0, -10, 10, -5, 0)
     love.graphics.pop()
 
-    -- love.graphics.polygon("line", asteroid.body:getWorldPoints(asteroid.shape:getPoints()))
 
-    if(gameOver) then
-    love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
+    -- draw asteroid
+    love.graphics.push()
+    love.graphics.circle("line", asteroid.body:getX(), asteroid.body:getY(), 20, 5)
+    love.graphics.pop()
+
+    if (gameOver) then
+        love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
     end
 
-    -- love.graphics.print("FPS: " .. tostring(p), 10, 10)
-    -- love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
-
-
+    love.graphics.print("bullets: " .. tostring(#player.bullets), 10, 10)
 end
